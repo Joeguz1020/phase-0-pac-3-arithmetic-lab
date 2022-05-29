2457dd35725c90d1972b0b3b3288e1f9e3cf829a
Joseph Guzman <joe.guzman614@yahoo.com>
2022-05-28 22:20:39 -0400

    commit tree adfc0b96d169e2a3ff2a6cc01643aad852ea7b45

---
+++ index.js
@@ -0,0 +1,56 @@
+function add(add) {
+    console.log(add);
+}
+function subtract(subtract) {
+    console.log(subtract);
+}
+function multiply(multiply) {
+    console.log(multiply);
+}
+function divide(divide) {
+    console.log(divide);
+}
+function add(a, b) {
+    console.log(a, b)
+    return a + b;
+}
+function subtract(a, b) {
+    console.log(a, b)
+    return a - b;
+}
+function multiply(a, b) {
+    console.log(a, b)
+    return a * b;
+}
+function divide (a, b) {
+    console.log (a, b)
+    return a / b;
+}
+function increment(n) {
+    console.log(n++)
+    return n++;
+}
+function decrement(n) {
+    console.log(n--)
+    return n--;
+}
+function makeInt(n) {
+    parseInt(n)
+    return n;
+}
+function makeInt(n) {
+    console.log(n)
+    return n;
+}
+function parseInt(n) {
+    parseInt(n, 10)
+    return n;
+}
+function preserveDecimal(n) {
+    parseFloat(n)
+    return n;
+}
+function makeInt(n, n) { 
+    console.log(n, n)
+    return n * n;
+}
